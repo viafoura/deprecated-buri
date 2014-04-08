fbd40e912e9eeb2850273893b246c8a567ce17ab
Joe Hohertz <joe@viafoura.com>
2014-04-08 13:04:43 -0400

    test script for ephemerals has mostly taken shape, just needs wrapping into init

--- playbooks/files/ephemeral-setup.sh
+++ playbooks/files/ephemeral-setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -e /dev/md0 ]; then
+  echo "Detected RAID already setup, exiting"
+  exit 0
+fi
+
 METADATA_URL_BASE="http://169.254.169.254/2012-01-12"
  
 # Configure Raid - take into account xvdb or sdb
@@ -13,72 +18,59 @@ else
   DRIVE_SCHEME='sd'
 fi
 
-reserved=0
- 
-# figure out how many ephemerals we have by querying the metadata API, and then:
-#  - convert the drive name returned from the API to the hosts DRIVE_SCHEME, if necessary
-#  - verify a matching device is available in /dev/
-raid_drives=()
-reserved_drives=()
 ephemeral_drives=()
 
-ephemeral_count=0
 ephemerals=$(curl --silent $METADATA_URL_BASE/meta-data/block-device-mapping/ | grep ephemeral)
 for e in $ephemerals; do
   echo "Probing $e .."
   device_name=$(curl --silent $METADATA_URL_BASE/meta-data/block-device-mapping/$e)
-  # might have to convert 'sdb' -> 'xvdb'
   device_name=$(echo $device_name | sed "s/sd/$DRIVE_SCHEME/")
   device_path="/dev/$device_name"
  
-  # test that the device actually exists since you can request more ephemeral drives than are available
-  # for an instance type and the meta-data API will happily tell you it exists when it really does not.
   if [ -b $device_path ]; then
     echo "Detected ephemeral disk: $device_path"
-    if [ $ephemeral_count -ge $reserved ]; then
-      raid_drives+=($device_path)
-    else 
-      reserved_drives+=($device_path)
-    fi
     ephemeral_drives+=($device_path)
-    ephemeral_count=$((ephemeral_count + 1 ))
   else
     echo "Ephemeral disk $e, $device_path is not present. skipping"
   fi
 done
 
-
-# 1 = device, 2 = mountpoint
-function format_and_mount() {
-	echo "Format/Mount $1 to $2";
-}
- 
-# 1 = list to raid, 2 = list to manage single
-function remake_volumes() {
-}
-
-case "$ephemeral_count" in
+case "${#ephemeral_drives[@]}" in
   0)
 	echo "No ephemeral disk detected. No action taken."
+	exit 0
         ;;
   1)
 	echo "Single ephemeral disk detected. No action taken."
-	#exit 0
-        ;;
-  2)
-	echo "Two ephemeral disks detected."
-	if [ ${reserved} != 0 ]; then
-		format_and_mount(${ephemeral_drives[1]}, "/mnt/reserved")
-		# Format/mount the 2nd ephemeral
-        else
-		remake_volumes("${ephemeral_drives[@]}", "")
-		# Unmount 1st ephemeral, make raid
-        fi
-	#exit 0
+	exit 0
         ;;
   *)
-	echo "Multiple ephemeral disks detected."
-        #exit 1
+	echo "Several (2+) ephemeral disks detected, raiding"
         ;;
 esac
+
+# ephemeral0 is typically mounted for us already. umount it here
+umount /mnt
+ 
+# overwrite first few blocks in case there is a filesystem, otherwise mdadm will prompt for input
+for drive in ${ephemeral_drives[@]}; do
+  dd if=/dev/zero of=$drive bs=4096 count=1024
+done
+ 
+partprobe
+mdadm --create --verbose /dev/md0 --level=0 -c256 --raid-devices=${#ephemeral_drives[@]} ${ephemeral_drives[@]}
+echo DEVICE ${ephemeral_drives[@]} | tee /etc/mdadm/mdadm.conf
+mdadm --detail --scan  | grep -oP '.*(?=\sname=.*)' | tee -a /etc/mdadm/mdadm.conf
+blockdev --setra 65536 /dev/md0
+mkfs -t ext4 /dev/md0
+mount -t ext4 -o noatime /dev/md0 /mnt
+echo $((30*1024)) > /proc/sys/dev/raid/speed_limit_min
+ 
+# Remove xvdb/sdb from fstab
+sed -i "/${DRIVE_SCHEME}b/d" /etc/fstab
+ 
+# Make raid appear on reboot
+echo "/dev/md0 /mnt ext4 noatime 0 0" | tee -a /etc/fstab
+
+update-initramfs -u
 
