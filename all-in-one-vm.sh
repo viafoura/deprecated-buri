405e7ae875b9482af16516244fa5ca89a720aff2
Joe Hohertz <joe@viafoura.com>
2014-07-02 09:08:41 -0400

    initial splitting of environment

--- all-in-one-vm.sh
+++ all-in-one-vm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-playbook playbooks/all-in-one-live.yml -i inventory/local -e "machine_target=$1" -vvvv
+ansible-playbook playbooks/all-in-one-live.yml -i playbooks/dev_vm/inventory -e "machine_target=$1" -vvvv
 
--- create-bundle-keys.sh
+++ create-bundle-keys.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-openssl genrsa -out playbooks/local/bundle_pk.pem 2048
-openssl req -new -x509 -sha1 -days 3750 -key playbooks/local/bundle_pk.pem -out playbooks/local/bundle_cert.pem -batch
+ENVIRO=$1
+if [ "x$ENVIRO" == "x" ]; then
+  echo "Must supply environment name"
+  exit 1
+fi
+BASE="playbooks/$ENVIRO/local"
+if [ ! -d "${BASE}" ]; then
+  echo "Invalid environment directory: $ENVIRO"
+  exit 1
+fi
+
+
+openssl genrsa -out ${BASE}/bundle_pk.pem 2048
+openssl req -new -x509 -sha1 -days 3750 -key ${BASE}/bundle_pk.pem -out ${BASE}/bundle_cert.pem -batch
 
