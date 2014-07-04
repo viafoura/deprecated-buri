db0076293a59c3460f718996c26376ec61ca112a
Joe Hohertz <jhohertz@gmail.com>
2014-07-04 01:28:23 -0400

    have all in one generate its own cassandra keys

--- all-in-one-vm.sh
+++ all-in-one-vm.sh
@@ -3,6 +3,7 @@
 SCRIPT_PATH=$(readlink -f $0)
 BURI_BASE=$(dirname ${SCRIPT_PATH})
 
+${BURI_BASE}/bin/create-cassandra-keys.sh dev_vm
 
 ansible-playbook ${BURI_BASE}/playbooks/all-in-one-live.yml -i ${BURI_BASE}/playbooks/dev_vm/inventory -e "machine_target=$1" -vvvv
 
