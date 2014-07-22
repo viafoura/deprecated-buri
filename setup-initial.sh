53cfd3c5e2fa13238b444cffd3d5a82f3f55f7d9
Joe Hohertz <joe@viafoura.com>
2014-07-22 12:29:53 -0400

    remove the java ec2-api-tools from being installed

--- setup-initial.sh
+++ setup-initial.sh
@@ -9,13 +9,12 @@ BURI_BASE=$(dirname ${SCRIPT_PATH})
 export DEBIAN_FRONTEND=noninteractive
 sudo perl -pi -e 's/^# *(deb .*multiverse)$/$1/' /etc/apt/sources.list
 sudo perl -pi -e 's/^# *(deb .*backports)$/$1/' /etc/apt/sources.list
-sudo add-apt-repository -y ppa:jhohertz/aws-ppa
+#sudo add-apt-repository -y ppa:jhohertz/aws-ppa
 sudo apt-get update
 sudo -E apt-get upgrade -y
 sudo -E apt-get install --no-install-recommends -y \
  coreutils                \
  ec2-ami-tools            \
- ec2-api-tools            \
  python                   \
  python-support           \
  python-dev               \
